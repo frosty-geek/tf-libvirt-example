641744887409eebb53a958d5b63775489645cc53
Ralf Heiringhoff <ralf@frosty-geek.net>
2022-03-11 00:56:37 +0100

    removed metadata

--- cloudinit.tf
+++ cloudinit.tf
@@ -2,6 +2,6 @@ resource "libvirt_cloudinit_disk" "cloudinit" {
   name      = "${var.hostname}-cloud-init.iso"
   pool      = var.volume_pool
   user_data = data.template_file.userdata.rendered
-  meta_data = data.template_file.metadata.rendered
+  #meta_data = data.template_file.metadata.rendered
   #network_config = data.template_file.networkconfig.rendered
 }
